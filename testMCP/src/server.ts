e3e215cbf300eb19a77c17aa1d49b35593b2ac0a
rtree <arakitomohiko@gmail.com>
2025-07-30 09:43:51 +0900

    ong

--- testMCP/src/server.ts
+++ testMCP/src/server.ts
@@ -1,3 +1,4 @@
+// PRJROOT/testMCP/src/server.ts
 import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
